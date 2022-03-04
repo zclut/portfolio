081bdb4fcf3cd73f1512346657e995e422c0d3d3
Juan <juandanielpadilla0@gmail.com>
2022-03-04 22:49:04 +0000

    :pushpin: | Portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './portfolio.css'
+import { data } from './data'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>Mi trabajo más reciente</h5>
+      <h2>Proyectos</h2>
+
+      <div className="container portfolio__container">
+        {data.map( ({id, image, title, github, demo}) => (
+          <article className="portfolio__item" key={id}>
+            <div className="portfolio__item-image">
+              <img src={image} alt="" />
+            </div>
+
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className='btn btn-primary' target="_blank">Github</a>
+            </div>
+          </article>
+        ))}
+      </div>
+
+    </section>
   )
 }
 
---
+++ src/components/portfolio/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id: 1,
+        image: "",
+        title: "Bar de Juan",
+        github: "https://github.com/zclut"
+    },
+    {
+        id: 2,
+        image: "",
+        title: "Finaccess",
+        github: "https://github.com/zclut"
+    }
+]
