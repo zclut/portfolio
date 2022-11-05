c66ff8fb3bcaa9a96b18087f59eb2dbeee374788
zClut <juandanielpadilla0@gmail.com>
2022-11-05 15:59:54 +0000

    :pushpin: | adding xalhoot to portfolio

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -2,30 +2,39 @@ import finaccess from '../../assets/portfolio/finaccess.png';
 import barDeJuan from '../../assets/portfolio/bardejuan.png';
 import graphqlAuthTemplate from '../../assets/portfolio/graphqlAuthTemplate.png';
 import karmaland from '../../assets/portfolio/karmaland.png';
+import xalhoot from '../../assets/portfolio/xalhoot.png';
 
 export const data = [
     {
         id: 1,
+        image: xalhoot,
+        title: "Xalhoot",
+        github: "https://github.com/zclut/xalhoot"
+    },
+    {
+        id: 2,
+        image: karmaland,
+        title: "Karmaland UI",
+        github: "https://github.com/klasinky/karmaland-ui"
+    },
+    {
+        id: 3,
         image: graphqlAuthTemplate,
         title: "GraphQL Auth Template",
         github: "https://github.com/zclut/graphql-auth-template"
     },
     { 
-        id: 2,
+        id: 4,
         image: barDeJuan,
         title: "Bar de Juan",
         github: "https://github.com/zclut/bardejuan-app"
     },
     {
-        id: 3,
+        id: 5,
         image: finaccess,
         title: "Finaccess",
         github: "https://github.com/zclut/FinaccessBackend"
     },
-    {
-        id: 4,
-        image: karmaland,
-        title: "Karmaland UI",
-        github: "https://github.com/klasinky/karmaland-ui"
-    },
+
+    
 ]
